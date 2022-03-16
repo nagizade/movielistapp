b2c1f4a1914c4af1da6c2a88331b554b751aab41
nagizade <rossokam@gmail.com>
2022-03-16 16:25:59 +0400

    importing dependencies

--- app/src/main/java/com/hasannagizade/movielistapp/presentation/tabs/main/MainActivity.kt
+++ app/src/main/java/com/hasannagizade/movielistapp/presentation/tabs/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hasannagizade.movielistapp
+package com.hasannagizade.movielistapp.presentation.tabs.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hasannagizade.movielistapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
